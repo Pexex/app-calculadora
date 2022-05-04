7006070b1ae3eef4d3339912b144bc63ab0c22bc
Roberto Peixinho <rhpeixinho@gmail.com>
2022-05-04 00:12:28 -0300

    Eventos tratados via Javascript

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// TODO descobrir porque não funcionou com eventlistener ou onclick no js
-
 // eventos para ações
-// document.getElementById("clear").onclick = calcular('acao', 'c');
-// document.getElementById("divide").onclick = calcular('acao', '/');
-// document.getElementById("multiplica").onclick = calcular('acao', '*');
-// document.getElementById("subtrai").onclick = calcular('acao', '-');
-// document.getElementById("soma").onclick = calcular('acao', '+');
-// document.getElementById("igual").onclick = calcular('acao', '=');
-// document.getElementById("ponto").onclick = calcular('acao', '.');
+document.getElementById("clear").onclick = function() {calcular('acao', 'c')};
+document.getElementById("divide").onclick = function() {calcular('acao', '/')};
+document.getElementById("multiplica").onclick = function() {calcular('acao', '*')};
+document.getElementById("subtrai").onclick = function(){calcular('acao', '-')} ;
+document.getElementById("soma").onclick = function() {calcular('acao', '+')};
+document.getElementById("igual").onclick = function() {calcular('acao', '=')};
+document.getElementById("ponto").onclick = function() {calcular('acao', '.')};
 
-// Eventos para valores
-// document.getElementById("zero").onclick = calcular('valor', 0);
-// document.getElementById("um").onclick = calcular('valor', 1);
-// document.getElementById("dois").onclick = calcular('valor', 2);
-// document.getElementById("tres").onclick = calcular('valor', 3);
-// document.getElementById("quatro").onclick = calcular('valor', 4);
-// document.getElementById("cinco").onclick = calcular('valor', 5);
-// document.getElementById("seis").onclick = calcular('valor', 6);
-// document.getElementById("sete").onclick = calcular('valor', 7);
-// document.getElementById("oito").onclick = calcular('valor', 8);
-// document.getElementById("nove").onclick = calcular('valor', 9);
+//Eventos para valores
+document.getElementById("zero").onclick = function() {calcular('valor', 0)};
+document.getElementById("um").onclick = function() {calcular('valor', 1)};
+document.getElementById("dois").onclick = function() {calcular('valor', 2)};
+document.getElementById("tres").onclick = function() {calcular('valor', 3)};
+document.getElementById("quatro").onclick = function() {calcular('valor', 4)};
+document.getElementById("cinco").onclick = function() {calcular('valor', 5)};
+document.getElementById("seis").onclick = function() {calcular('valor', 6)};
+document.getElementById("sete").onclick = function() {calcular('valor', 7)};
+document.getElementById("oito").onclick = function() {calcular('valor', 8)};
+document.getElementById("nove").onclick = function() {calcular('valor', 9)};
 
 var display = document.getElementById("resultado");
 
